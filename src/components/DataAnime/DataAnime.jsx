0dd34f4f8a7bf3e6f73c599915a8348cf25d5c4e
Erlin Dominguez <erlind299@gmail.com>
2023-06-16 14:37:32 -0500

    Adding information of anime, noticies and trailer

--- src/components/DataAnime/DataAnime.jsx
+++ src/components/DataAnime/DataAnime.jsx
@@ -1,19 +1,44 @@
 import { useLocation, Link } from 'react-router-dom';
+import "../DataAnime/dataAnime.css"
 
 function DataAnime() {
   const location = useLocation();
-  const anime = location.state.anime;
-
+  const item = location.state.item;
+console.log(item);
   return (
-    <div>
+    <>
       <h2>DataAnime</h2>
       <Link to="/">Volver al inicio</Link>
-      {anime && (
-        <div>
-          <p>hola</p>
-        </div>
-      )}
-    </div>
+      <div className='content_data'>
+        {item && (
+          <div>
+            <div>
+              <p>Nombre: {item.title}</p>
+              <p>Trailer</p>
+            </div>
+            <div className='side-info'>
+              <div>
+                <iframe src={item.trailer.embed_url} frameBorder="0" width={735} height={470} className='iframe-container'></iframe>
+              </div>
+              <div className='noticies'>
+                <p>Noticias</p>
+                {
+               Array(5).fill().map(() => (
+                // eslint-disable-next-line react/jsx-key
+                <div className='box-notice'>
+                  <img src={item.images.jpg.large_image_url} alt="" className='image_notice'/>
+                  <div>
+                    <p>Total de episodios: {item.episodes}</p>
+                    <p>{item.source}</p>
+                  </div>
+                </div>
+              ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
