29239b3f03244340dcbed8c9674c82987252c8a4
Erlin Dominguez <erlind299@gmail.com>
2023-06-16 14:36:27 -0500

    Adding click event of topAnimes

--- src/components/TopAnime/TopAnime.jsx
+++ src/components/TopAnime/TopAnime.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/jsx-key */
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../TopAnime/topAnime.css'
 import axios from 'axios';
 
 function TopAnime() {
   const [topAnime, setTopAnime] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchTopAnime();
@@ -15,12 +17,16 @@ function TopAnime() {
     setTopAnime(response.data.data);
   }
 
+  const handleClick = (item) => {
+    navigate(`/data-anime`, { state: { item } });
+  }
+
   return (
     <>{}
       <div className='nav-popular'>
         <h3>Más pupular</h3>
         {topAnime.map((item) => (
-          <p key={item.mal_id}>{item.title}</p>
+          <p key={item.mal_id} onClick={() => handleClick(item)}>{item.title}</p>
         ))}
       </div>
     </>
