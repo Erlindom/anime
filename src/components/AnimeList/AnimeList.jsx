7fe17ffdfdde0ca1ae2745438f3b39eb26c858c2
Erlin Dominguez <erlind299@gmail.com>
2023-06-15 23:31:42 -0500

    Adding search

--- src/components/AnimeList/AnimeList.jsx
+++ src/components/AnimeList/AnimeList.jsx
@@ -6,18 +6,29 @@ import TopAnime from '../TopAnime/TopAnime';
 
 function AnimeList() {
   const [animeList, setAnimeList] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetchAnime()
-  }, [])
+  }, [search])
 
   async function fetchAnime() {
-    const response = await axios.get("https://api.jikan.moe/v4/anime")
+    const response = await axios.get(`https://api.jikan.moe/v4/anime?q=${search}`)
     setAnimeList(response.data.data);
+  }
+
+  const animeSearch = (event) => {
+    const searcher = event.target.value;
+    setSearch(searcher);
   }
 
   return (
     <>
+    <div className="search-box">
+      <input type="search" placeholder="Search your anime"
+        onChange={animeSearch}
+      />
+    </div>
     <div className='contentpro'>
     <TopAnime />
       <div className='anime_container'>
