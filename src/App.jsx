1a7a8bb6abf01c7c4afdc69cb21fee4e54d5cc2c
Erlin Dominguez <erlind299@gmail.com>
2023-06-16 00:32:15 -0500

    Dependencies react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
-import AnimeList from './components/AnimeList/AnimeList'
+import './App.css';
+import AnimeList from './components/AnimeList/AnimeList';
+import DataAnime from './components/DataAnime/DataAnime';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    <AnimeList />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<AnimeList />} />
+        <Route path="/data-anime" element={<DataAnime />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
